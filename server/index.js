789a2a3c037e5b85f61f88251df0d59002933047
obaid <obaidlone@gildwaretechnologies.com>
2023-11-30 23:07:59 +0530

    creating backend apis

--- server/index.js
+++ server/index.js
@@ -1,20 +1,27 @@
 import express from "express";
-import bodyParser  from "body-parser";
-import mongoose from "mongoose"
-import dotenv from "dotenv"
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import multer from "multer";
-import helmet from "helmet"
+import helmet from "helmet";
 import morgan from "morgan";
-import path from 'path'
-import cors from "cors"
+import path from "path";
+import cors from "cors";
 import { fileURLToPath } from "url";
 
+import { register } from "./controllers/auth.js";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import { verifyToken } from "./middleware/auth.js";
+import {createPost} from "./controllers/posts.js"
+import postRoutes from "./routes/posts.js"
 
 
 //Configuration
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+``;
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -26,27 +33,39 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-
-
-
 /* FILE STORAGE */
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "public/assets");
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname);
-    },
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+// Routes with Files
+
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts",verifyToken, upload.single("picture"), createPost);
+
+//Routes
+
+app.use("/auth", authRoutes);
+app.use("/user",userRoutes)
+app.use("/posts",postRoutes)
+
+//Mongoose Setup
+
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server is Running On ${PORT}`));
+  })
+  .catch((err) => {
+    console.log(`${PORT} not connected`);
   });
-  const upload = multer({ storage });
-
-//Monoose Setup 
- 
-const PORT=process.env.PORT || 6001;
-mongoose.connect(process.env.MONGO_URL,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}).then(()=>{
-    app.listen(PORT,()=>console.log(`Server is Running On ${PORT}`))
-}).catch((err)=>{console.log(`${PORT} not connected`)})
-
