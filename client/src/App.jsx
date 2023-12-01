c201b13961fd3e03724104fc02bd1b28f9ef7af1
obaid <obaidlone@gildwaretechnologies.com>
2023-12-01 18:53:19 +0530

    setting redux toolkit and theme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,31 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./scenes/LoginPage";
+import HomePage from "./scenes/HomePage";
+import ProfilePage from "./scenes/ProfilePage";
+import { useSelector } from "react-redux";
+import { useMemo } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+
+
 function App() {
-
-
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
-    <>
-  
-    </>
-  )
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />}></Route>
+            <Route path="/home" element={<HomePage/>}></Route>
+            <Route path="/profile/:userId" element={<ProfilePage />}></Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
