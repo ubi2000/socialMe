e3205f3fe6bc7dcc3ef03dba6e0f55ab589a2f3b
obaid <obaidlone@gildwaretechnologies.com>
2023-12-03 13:10:27 +0530

    checking dep

--- client/src/scenes/Form.jsx
+++ client/src/scenes/Form.jsx
@@ -65,7 +65,7 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-      "http://localhost:3000/auth/register",
+      "https://social-me-backend-eight.vercel.app/auth/register",
       {
         method: "POST",
         body: formData,
@@ -81,7 +81,7 @@ const Form = () => {
 
   //Login
   const login = async (values, onSubmitProps) => {
-    const loggedInResponse = await fetch("http://localhost:3000/auth/login", {
+    const loggedInResponse = await fetch("https://social-me-backend-eight.vercel.app/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
